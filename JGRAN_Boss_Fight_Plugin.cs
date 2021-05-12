9303e5a630e604f9430212ad54e8c6a512c2dc9d
Rondeo Balos <mr.ron669@gmail.com>
2021-05-12 09:37:30 +0800

    initial commit/release

---
+++ JGRAN_Boss_Fight_Plugin.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Reflection;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace JGRAN_Boss_Fight_Plugin
+{
+    [ApiVersion(2, 1)]
+    public class JGRAN_Boss_Fight_Plugin : TerrariaPlugin
+    {
+        public override string Author => "Rondeo Balos";
+        public override string Description => "A Boss Fight plugin";
+        public override string Name => "JGRAN Boss Fight Plugin";
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+        public JGRAN_Boss_Fight_Plugin(Main game) : base(game) { }
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.GameInitialize.Register(this, onInit);
+            ServerApi.Hooks.NpcAIUpdate.Register(this, onNPCAIUpdate);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GameInitialize.Deregister(this, onInit);
+                ServerApi.Hooks.NpcAIUpdate.Deregister(this, onNPCAIUpdate);
+            }
+            base.Dispose(disposing);
+        }
+
+        void onInit(EventArgs args)
+        {
+            Commands.ChatCommands.Add(new Command("tshock.admin.bossfight", bossFightCommand, "bossfight")
+            {
+                HelpText = "JGRAN Reward Plugin"
+            });
+        }
+
+        float[] bosspos = null;
+        int radius = 40000;
+
+        const int X = 0;
+        const int Y = 1;
+
+        void bossFightCommand(CommandArgs args)
+        {
+            var token = args.Parameters[0];
+            switch (token)
+            {
+                case "position":
+                    // bossfight position
+                    bosspos = new float[] { args.Player.LastNetPosition.X, args.Player.LastNetPosition.Y };
+                    args.Player.SendSuccessMessage($"Boss Fight position has been set to [{bosspos[0]},{bosspos[1]}]!");
+                    break;
+                case "radius":
+                    //bossfight radius 50000
+                    if (Int32.TryParse(args.Parameters[1], out radius))
+                    {
+                        args.Player.SendSuccessMessage($"Boss Fight radius has been set to {radius}!");
+                    }
+                    break;
+            }
+        }
+
+        void onNPCAIUpdate(NpcAiUpdateEventArgs args)
+        {
+            if (args.Npc.boss && bosspos != null)
+            {
+                //Console.WriteLine($"boss position: [{args.Npc.position.X},{args.Npc.position.Y}] region position: [{bosspos[X]},{bosspos[Y]}]");
+                if(args.Npc.position.X < bosspos[X] - radius && args.Npc.position.X > bosspos[X] + radius)
+                {
+                    Console.WriteLine("Boss out of range");
+                    args.Npc.DirectionTo(new Microsoft.Xna.Framework.Vector2(bosspos[X],bosspos[Y]));
+                }
+            }
+        }
+    }
+}
